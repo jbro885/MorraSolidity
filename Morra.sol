1a86014a16f5cf72aafc565a5f9bed4a80f5e048
Michael Michaelides <michaelides.michael.1193.g2@gmail.com>
2019-11-06 13:38:37 +0000

    gas efficiency and fairness

--- Morra.sol
+++ Morra.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.4.22 <0.6.0;
 /**
  * An ethereum contract for a Morra game between two people
- * Address: 0xa7dd711CDD846039A9D14A5643b936346BC8E2c7
+ * Address: 0x62903D319Cd0BBB7CB4eC44906b03F4152287Dff
  */
 contract Morra {
     struct guess {
@@ -9,10 +9,10 @@ contract Morra {
         address player;
         // the number chosen for themself, after revealing.
         uint8 myNumber;
-        // the hash of the number chosen for themself, after revealing, in the form of a hashed string that ends with the desired number
-        bytes32 hashMyNumber;
         // the number guessed for the other player's value, after revealing
         uint8 guessedNumber;
+        // the hash of the number chosen for themself, after revealing, in the form of a hashed string that ends with the desired number
+        bytes32 hashMyNumber;
         // the hash of the number guessed, after revealing, in the form of a hashed string that ends with the desired number
         bytes32 hashGuessedNumber;
     }
@@ -20,17 +20,20 @@ contract Morra {
     mapping (address => uint256) refunds;
     // records (up to) two game entries for each round
     guess[] public guesses;
+    // history of last game to allow players to see the results by themselves
+    guess[2] public history;
     // value of guess.myNumber and guessedNumber before revealing
     uint8 private constant NOT_RVLD = 6;
+    // this is how long a round lasts before it expires and can be terminated manually
+    uint128 public constant maxTimeTillGameExpiry = 1 hours;
+    uint256 public constant REVEAL_GAS = 100000 wei;
     // how much ether is needed to play a round
     uint256 public constant gamePrice = 5 * gamePriceUnit;
     uint256 private constant gamePriceUnit = 1 ether;
     // stores the time both players committed for this round. Used to avoid non revealing players stalling the game
-    uint256 private timeOfGame = 0;
-    // this is how long a round lasts before it expires and can be terminated manually
-    int256 public constant maxTimeTillGameExpiry = 1 days;
+    uint256 private timeOfGame;
     // notifies the players that there is a reward for them that can be retreived using collect
-    event CanCollect(address player);
+    event CanCollect(address player, uint256 value);
     // notifies the players of this round that both of them have committed so they should now reveal
     event CanReveal(address player);
 
@@ -54,9 +57,9 @@ contract Morra {
     /**
      * Given a string that ends in a number 0-9, return that number as an int.
      * This is possible because numbers in UTF-8 are single byte long.
-     * If the last character is not an integer in the range [1,5] then return 0 indicating invalid string
+     * @return the last char of the string as an int or 0 if it's not an integer between [1,5]
      */
-    function getNumber(string memory textEndingWithNumber) internal pure returns (uint8 ret) {
+    function getNumber(string memory textEndingWithNumber) internal pure returns (uint8) {
         bytes memory bstring = bytes(textEndingWithNumber);
         byte last = bstring[bstring.length - 1];
         if (last == "1") return 1;
@@ -70,44 +73,49 @@ contract Morra {
 
     /**
      * checks if the given player has won the round by guessing the other player's picked number
+     * @return if the player at index playerIndex has guessed correctly the other player's number
      */
-    function didPlayerWin(uint8 playerIndex) internal view returns (bool) {
-        uint8 otherIndex = (playerIndex + 1) % 2;
+    function didPlayerWin(uint256 playerIndex) internal view returns (bool) {
+        uint256 otherIndex = (playerIndex + 1) % 2;
         return guesses[playerIndex].guessedNumber == guesses[otherIndex].myNumber;
     }
 
     /**
      * Distributes the rewards at the end of the round, as follows:
-     *  if both players won: return to each the price they payed to play
+     *  if both players won or both lost: return to each the price they payed to play
      *  if only one of them won: return to the winner the their reward, i.e. the sum of the two numbers
      *      guessed by each player plus the difference between the guessed number and what they've payed to play
-     *  if no player won: they both get nothing back
      */
     function distributeReward() internal {
-        assert(address(this).balance >= 2 * gamePrice);
+        uint256 balance = address(this).balance;
+        assert(balance >= 2 * gamePrice);
         assert(guesses.length == 2);
         assert(guesses[0].myNumber != NOT_RVLD && guesses[1].myNumber != NOT_RVLD);
-        uint256 sum = (guesses[0].guessedNumber + guesses[1].guessedNumber) * gamePriceUnit;
-        assert(sum <= 2 * gamePrice);
+        // caching into memory storage variables that will be used below, to be gas efficient
         bool player0won = didPlayerWin(0);
         bool player1won = didPlayerWin(1);
-        if (player0won && player1won) {
-            refunds[guesses[0].player] = gamePrice;
-            refunds[guesses[1].player] = gamePrice;
+        uint256 winnerPlayerIndex = player0won ? 0 : 1;
+        uint256 looserPlayerIndex = (winnerPlayerIndex + 1) % 2;
+        address winner = guesses[winnerPlayerIndex].player;
+        address looser = guesses[looserPlayerIndex].player;
+        if ((player0won && player1won) || (!player0won && !player1won)) {
+            uint256 refund = gamePrice + REVEAL_GAS/2;
+            refunds[winner] = refund;
+            refunds[looser] = refund;
             // notify players to collect
-            emit CanCollect(guesses[0].player);
-            emit CanCollect(guesses[1].player);
-        } else if (player0won) {
+            emit CanCollect(winner, refund);
+            emit CanCollect(looser, refund);
+        } else {
+            uint256 sum = (guesses[0].guessedNumber + guesses[1].guessedNumber) * gamePriceUnit;
             // refund the sum plus any surplus from the difference between price of game and the value paid by winning player
-            refunds[guesses[0].player] = (gamePrice - guesses[0].guessedNumber * gamePriceUnit) + sum;
-            // notify player to collect
-            emit CanCollect(guesses[0].player);
-            refunds[guesses[1].player] = 0;
-        } else if (player1won) {
-            refunds[guesses[0].player] = 0;
-            refunds[guesses[1].player] = (gamePrice - guesses[1].guessedNumber * gamePriceUnit) + sum;
-            // notify player to collect
-            emit CanCollect(guesses[1].player);
+            uint256 refundWinner = (gamePrice - guesses[winnerPlayerIndex].guessedNumber * gamePriceUnit) + sum + REVEAL_GAS/2;
+            refunds[winner] = refundWinner;
+            emit CanCollect(winner, refundWinner);
+            uint256 refundLooser = (gamePrice - guesses[looserPlayerIndex].guessedNumber * gamePriceUnit) + REVEAL_GAS/2;
+            assert(refundWinner+refundLooser <= balance);
+            refunds[looser] = refundLooser;
+            emit CanCollect(looser, refundLooser);
+
         }
     }
 
@@ -128,8 +136,8 @@ contract Morra {
         bool p1 = guesses[1].myNumber != NOT_RVLD;
         // if only one has already revealed, make them a winner
         if (p0 != p1) {
-            uint8 revealedPlayerIndex = p0 ? 0 : 1;
-            uint8 unrevealedPlayerIndex = (revealedPlayerIndex + 1) % 2;
+            uint256 revealedPlayerIndex = p0 ? 0 : 1;
+            uint256 unrevealedPlayerIndex = (revealedPlayerIndex + 1) % 2;
             // manually set the choices of the player who didn't reveal, in the most favorable way to
             // the player who revealed.
             uint8 manualGuess = guesses[revealedPlayerIndex].myNumber == 5 ? 4 : 5;
@@ -167,14 +175,14 @@ contract Morra {
     /*
      * Allows a player to enter this round of the game by submitting their choices as hashed values.
      * Players need to pay the game price to play, which will be returned along with a prize in the case of a win.
-     * @param hashValue: hash of a string ending with a desired number [1,5], e.g. abcd3, where the number represents
+     * @param hashMyNumber: hash of a string ending with a desired number [1,5], e.g. abcd3, where the number represents
      *  the player's selected number
-     * @param hashOtherValue: hash of a string ending with a desired number [1,5], e.g. cdg2, where the number represents
+     * @param hashGuessedNumber: hash of a string ending with a desired number [1,5], e.g. cdg2, where the number represents
      *  the other guess of the other player's number
      *
-     * P.S. you can use https://emn178.github.io/online-tools/keccak_256.html to calculate hash
+     * P.S. you can use https://emn178.github.io/online-tools/keccak_256.html to calculate hashes
     */
-    function submitGuess(bytes32 hashValue, bytes32 hashOtherValue) public payable {
+    function submitGuess(bytes32 hashMyNumber, bytes32 hashGuessedNumber) external payable {
         //if the previous game expired and nobody terminated yet, end the previous game to allow the next round to go on.
         if (isGameExpired()) {
             restartGame();
@@ -182,8 +190,8 @@ contract Morra {
         require(guesses.length < 2, 'Both players have already played');
         require(msg.value >= gamePrice, 'Need to pay 5 ether to play. Nothing is free');
         // register player's guess, with specific not-yet-revealed values for the two numbers
-        uint newLength = guesses.push(guess(msg.sender, NOT_RVLD, hashValue, NOT_RVLD, hashOtherValue));
-        if (newLength == 2) {
+        uint numPlayers = guesses.push(guess(msg.sender, NOT_RVLD, NOT_RVLD, hashMyNumber, hashGuessedNumber));
+        if (numPlayers == 2) {
             // notify players to reveal
             emit CanReveal(guesses[0].player);
             emit CanReveal(guesses[1].player);
@@ -195,15 +203,19 @@ contract Morra {
      * Reveals a player's choice of the two strings that end with the desired numbers, whose hashes have been
      *   submitted when entering the game.
      * Should be a player who is playing for this round and the values should match their hashes
-     * @param revealedValue: value of the hashed string that represents this player's choice of own number
-     * @param otherRevealedValue: value of the hashed string that represents this player's guess of the other player's choice of own number
+     * @param myNumber: value of the hashed string that represents this player's choice of own number
+     * @param guessedNumber: value of the hashed string that represents this player's guess of the other player's choice of own number
      */
-    function reveal(string memory revealedValue, string memory otherRevealedValue) public {
+    function reveal(string calldata myNumber, string calldata guessedNumber) external payable {
         require(guesses.length == 2, 'Both players need to commit first');
         if (msg.sender == guesses[0].player) {
-            revealPlayer(guesses[0], revealedValue, otherRevealedValue);
+            if (guesses[1].myNumber == NOT_RVLD) require(msg.value >= REVEAL_GAS);
+            revealPlayer(guesses[0], myNumber, guessedNumber);
+            history[0] = guesses[0];
         } else if (msg.sender == guesses[1].player) {
-            revealPlayer(guesses[1], revealedValue, otherRevealedValue);
+            if (guesses[0].myNumber == NOT_RVLD) require(msg.value >= REVEAL_GAS);
+            revealPlayer(guesses[1], myNumber, guessedNumber);
+            history[1] = guesses[1];
         } else {
             require(false, 'Wait for the next round to play.');
         }
@@ -214,17 +226,22 @@ contract Morra {
         }
     }
 
+    /**
+     * @return true if the current game is expired else return false
+     */
     function isGameExpired() public view returns (bool) {
-        if (timeOfGame == 0) return false;
-        return maxTimeTillGameExpiry < int(now - timeOfGame);
+        uint256 timeOfGameMem = timeOfGame;
+        if (timeOfGameMem == 0) return false;
+        return maxTimeTillGameExpiry < int(now - timeOfGameMem);
     }
 
     /**
      * Allows a player to withdraw their rewards, if they have any
      * If the caller is a player of the current round and the game is expired,
      * then the current game is terminated, giving the chance to any revealed player to collect.
+     * @return amount of wei transfered
      */
-    function collect() public returns (uint256) {
+    function collect() external returns (uint256) {
         if (isGameExpired() && (msg.sender == guesses[0].player || msg.sender == guesses[1].player)) {
             endExpiredGame();
         }
